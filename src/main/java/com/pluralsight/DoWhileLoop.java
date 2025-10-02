e2776c7cdfefce40a2cb2a18f2d94a02c085bfd0
notLogi <92322066+notLogi@users.noreply.github.com>
2025-10-02 11:12:29 -0400

    Exercise 1 Complete

--- src/main/java/com/pluralsight/DoWhileLoop.java
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -5,7 +5,7 @@ public class DoWhileLoop {
         System.out.println("Do While Loop \n\n");
         int i = 0;
         do{
-            System.out.println("I love Java");
+            System.out.println("I love Java!");
             i++;
         }
         while(i < 5);
